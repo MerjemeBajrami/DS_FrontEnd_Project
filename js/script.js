64681ae5e161750b4b50f38738ccfbc33017747f
elmashatri <94225968+elmashatri@users.noreply.github.com>
2024-11-19 18:58:28 +0100

    Animate Sections

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ document.querySelector(".edit-button").addEventListener("click", () => {
     if (status) document.querySelector(".status").textContent = status;
     if (bio) document.querySelector(".bio-box").textContent = bio;
 });
+
+//Add animations for profile or notifications when the user interacts with them.
+document.querySelector(".profile").style.opacity = 0;
+document.querySelector(".profile").style.transition = "opacity 1s";
+setTimeout(() => {
+    document.querySelector(".profile").style.opacity = 1;
+}, 500);
